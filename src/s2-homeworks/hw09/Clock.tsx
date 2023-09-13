39acbce442564b92e8c2be73e555632bdafbf95e
admalla <maasovec@gmail.com>
2023-09-13 16:13:09 +0300

    fix hw9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,13 +34,11 @@ function Clock() {
 
     const stringTime =
         (
-            `${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}`
+            `${date.getHours() < 10 ? '0' + date.getHours().toLocaleString('ru-Ru') : date.getHours().toLocaleString('ru-Ru')}:${date.getMinutes() < 10 ? '0' + date.getMinutes().toLocaleString('ru-Ru') : date.getMinutes().toLocaleString('ru-Ru')}:${date.getSeconds() < 10 ? '0' + date.getSeconds().toLocaleString('ru-Ru') : date.getSeconds().toLocaleString('ru-Ru')}`
         ) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
 
     const stringDate =
-        (`${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}.
-    ${(date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)}.
-    ${date.getFullYear()}`) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+        (`${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}.${(date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)}.${date.getFullYear()}`) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat("en-US", {weekday: "long",})
